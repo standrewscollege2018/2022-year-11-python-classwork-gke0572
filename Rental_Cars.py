83fc61a7fe523262a8fd5fc36aa3678c765fc4d1
gke0572 <gke0572@stacmail.net>
2022-03-10 09:36:31 +1300

    Still need to fix so when all are booked the code stops running

--- Rental_Cars.py
+++ Rental_Cars.py
@@ -22,6 +22,7 @@ while main_loop == True:
             print(f"{i+1}. {cars[i][0]} {cars[i][1]}")
 
     #Choose a vehicle to book and checks that it is valid
+    booked_cars = 0
     check_number_vehicle = True
     while check_number_vehicle == True:
         try:
@@ -29,28 +30,31 @@ while main_loop == True:
             if number_vehicle == 0:
                 main_loop = False
                 check_number_vehicle = False
-            elif cars[number_vehicle - 1][2] == True:
-                print("** This vehicle is already booked. Please choose another.**")
-                check_number_vehicle = False
-            else:
-                if number_vehicle >= 10:
+            elif number_vehicle > 9:
                     print("Please enter the number of the car you would like to rent.")
-                elif number_vehicle < 0:
+            elif number_vehicle < 0:
                     print("Please enter a valid integer.")
+            else:
+                if cars[number_vehicle-1][2] == True:
+                    print("** This vehicle is already booked please choose another.**")
                 else:
                     #Prints a message as too which vehicle was booked
                     number_vehicle = number_vehicle - 1
                     cars[number_vehicle][2] = True
+                    booked_cars = booked_cars + 1
                     print(f"You have booked the {cars[number_vehicle][0]}.")
 
                     #Asks for name and adds too list, thanks name
+                    check_characters = False
                     check_name = True
                     while check_name == True:
                         name = input("What is your name? ")
                         if name.isdigit():
                             print("Please enter a valid name.")
+                            check_characters = True
                         elif name.replace(" ", "") == "":
                             print("Please enter a valid name.")
+                            check_characters = True
                         else:
                             check_characters = False
                             for character in name:
@@ -64,6 +68,7 @@ while main_loop == True:
                             check_name = False
                         else:
                             pass
+                    name = name.title()
                     cars[number_vehicle][3] = name
                     print(f"Thanks {name}")
                     check_number_vehicle = False
